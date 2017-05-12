4f2f8b3a643d4ab2ae4a85797e053866491235f9
Florian Matrion <florian.matrion@gmail.com>
2017-05-12 19:44:19 +0200

    init

---
+++ php7.1/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+unison_container="php71_unison_1";
+
+docker-compose up --force-recreate -d
+
+unison_port=`docker port $unison_container`
+unison_port=`echo $unison_port | cut -d':' -f 2`
+
+docker_machine_ip=`docker-machine ip`
+
+unison ./www socket://$docker_machine_ip:$unison_port/ -ignore 'Path .git' -auto -batch -repeat=watch
