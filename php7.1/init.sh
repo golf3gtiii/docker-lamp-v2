f3bdfadb130c39b197f54903ac653849523806bf
Florian Matrion <florian.matrion@gmail.com>
2017-05-24 21:06:01 +0200

    lancement unison - récupérer le nom du répertoire courant pour en déduire le nom du container docker

--- php7.1/init.sh
+++ php7.1/init.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-unison_container="php71_unison_1";
+#récupérer le nom du répertoire courant
+CURRENT_PATH="echo `pwd`"
+CURRENT_DIR=(`echo $CURRENT_PATH | sed 's/\//\n/g'`)
+CURRENT_DIRNAME=`echo ${CURRENT_DIR[-1]/./}`
+CURRENT_DIRNAME+="_unison_1"
 
 docker-compose up --force-recreate -d
 
+#récupérer le port utilisé par unison
+unison_container=$CURRENT_DIRNAME
 unison_port=`docker port $unison_container`
 unison_port=`echo $unison_port | cut -d':' -f 2`
 
+#récupérer l'ip de la machine docker
 docker_machine_ip=`docker-machine ip`
 
+#lancer unison
 unison ./www socket://$docker_machine_ip:$unison_port/ -ignore 'Path .git' -auto -batch -repeat=watch
